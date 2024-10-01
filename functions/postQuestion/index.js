bb16a5f03d4435d10c4061126331239f5d19108c
JensBrandels <brandelsjens@gmail.com>
2024-10-01 16:51:46 +0200

    added postQuestions that seems to be working

---
+++ functions/postQuestion/index.js
@@ -0,0 +1,88 @@
+const { v4: uuidv4 } = require("uuid");
+const { validateToken } = require("../../middleware/auth");
+const { sendResponse, sendError } = require("../../responses/index");
+const middy = require("@middy/core");
+const { db } = require("../../services/db");
+
+const handler = middy()
+  .use(validateToken)
+  .handler(async (event) => {
+    try {
+      const userId = event.userId;
+      const { quizId, question, answer, location } = JSON.parse(event.body);
+
+      //making sure we have all we need
+      if (
+        !quizId ||
+        !question ||
+        !answer ||
+        !location ||
+        !location.longitude ||
+        !location.latitude
+      ) {
+        return sendError(400, "Missing required fields");
+      }
+
+      //find the quiz inside table here
+      const getParams = {
+        TableName: "QuizTable",
+        Key: {
+          quizId: quizId,
+        },
+      };
+
+      const quizData = await db.get(getParams);
+      const quiz = quizData.Item;
+
+      if (!quiz) {
+        return sendError(404, "Quiz not found");
+      }
+
+      //check so that the owner of the quiz is the same as the user trying to make question
+      if (quiz.quizOwner !== userId) {
+        return sendError(
+          403,
+          "Permission Denied: You are not the owner of this quiz."
+        );
+      }
+
+      //Make the new question
+      const newQuestion = {
+        questionId: uuidv4(),
+        question: question,
+        answer: answer,
+        location: {
+          longitude: location.longitude,
+          latitude: location.latitude,
+        },
+      };
+
+      //add new question to existing array
+      const updatedQuestions = [...quiz.questions, newQuestion];
+
+      //Update the existing quiz inside the QuizTable
+      const updateParams = {
+        TableName: "QuizTable",
+        Key: {
+          quizId: quizId,
+        },
+        UpdateExpression: "set questions = :questions",
+        ExpressionAttributeValues: {
+          ":questions": updatedQuestions,
+        },
+        ReturnValues: "UPDATED_NEW",
+      };
+
+      await db.update(updateParams);
+
+      return sendResponse(200, {
+        message: "Question added successfully!",
+        updatedQuiz: updatedQuestions,
+      });
+    } catch (error) {
+      console.error("Detailed error", error);
+      return sendError(500, "Internal Server Error");
+    }
+  });
+
+module.exports = { handler };
